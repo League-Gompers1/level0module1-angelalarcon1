a8a1dd66b1e14244ed7635011e2bc38b9c1437a8
angelalarcon1 <angel.alarcon@jointheleague.org>
2017-05-15 14:51:06 -0700

    Add files via upload

---
+++ ForLoop_Gaultlant.java
@@ -0,0 +1,34 @@
+package for_loops;
+
+public class ForLoop_Gaultlant {
+	public static void main(String [] args){
+		
+		//(int i=0; i <= 100; i=i+1){
+			//System.out.println(i);
+			//	}
+
+		//for(int i=100; i >= 0; i=i-1){
+		//	System.out.println(i);
+			//	}
+
+		
+	//	for(int i=2; i >= 0; i=i+2){
+	//System.out.println(i);
+	//	}
+
+		//for(int i = 1; i <= 99; i = i+2){
+		//	System.out.println(i);
+		//}
+		
+for(int i = 0; i <= 10; i= i + 1){
+	System.out.print(i + " ");
+if(i% 2==0){
+	System.out.println("even");
+}else{
+	System.out.println("odd");
+	
+
+}
+}
+}
+}
