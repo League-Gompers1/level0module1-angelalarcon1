91838a8d49d8013912a222bbaae0a536d17a5788
angelalarcon1 <angel.alarcon@jointheleague.org>
2017-05-15 14:57:06 -0700

    Add files via upload

--- ForLoop_Gaultlant.java
+++ ForLoop_Gaultlant.java
@@ -20,15 +20,20 @@ public class ForLoop_Gaultlant {
 		//	System.out.println(i);
 		//}
 		
-for(int i = 0; i <= 10; i= i + 1){
-	System.out.print(i + " ");
-if(i% 2==0){
-	System.out.println("even");
-}else{
-	System.out.println("odd");
+//for(int i = 0; i <= 10; i= i + 1){
+	//System.out.print(i + " ");
+//if(i% 2==0){
+//	System.out.println("even");
+//}else{
+	//System.out.println("odd");
 	
+//for (int i=7; i<=777; i=i+7){
+//}
 
-}
-}
+	for(int i = 0; i<=12; i = i +1)	{
+		System.out.println(" In " +( i + 2005) "I was 12");
+	}
+		
+		
 }
 }
